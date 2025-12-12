6ac36594d9e69a12d29580b69a5704756faccc70
Yunus Eren <yeren@student.tgm.ac.at>
2025-12-12 13:01:03 +0100

    line of code deleted

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-d1380","appId":"1:230070896458:web:38f4f03c8b1e4759ad23b3","storageBucket":"friendlychat-d1380.firebasestorage.app","apiKey":"AIzaSyCxh9G57OE80dulw2gsotJPZ5QrhHixtpE","authDomain":"friendlychat-d1380.firebaseapp.com","messagingSenderId":"230070896458","projectNumber":"230070896458","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+      provideMessaging(() => getMessaging()),
+    provideRouter(routes)
   ],
 };
